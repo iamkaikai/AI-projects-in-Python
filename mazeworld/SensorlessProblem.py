fe34abcd9757393263b7c739ece89d2f7d42f08e
iamkaikai <iamkaikai@livemail.tw>
2023-09-24 19:56:09 -0400

    adding init state generation

--- mazeworld/SensorlessProblem.py
+++ mazeworld/SensorlessProblem.py
@@ -11,6 +11,23 @@ class SensorlessProblem:
         string =  "Blind robot problem: "
         return string
 
+    def generate_start_state(self):
+        map = self.maze.map
+        map_w = self.maze.width
+        map_h = self.maze.height
+        print(map_w, map_h)
+        count = 0
+       
+       
+        for i in map:
+            x = count % map_w
+            y = map_h - (count // map_w) -1
+            print(x,y)
+            # if i == '.':
+                
+            count += 1
+       
+        
     def animate_path(self, path):
         # reset the robot locations in the maze
         self.maze.robotloc = tuple(self.start_state)
@@ -29,4 +46,4 @@ if __name__ == "__main__":
     test_maze3 = Maze("maze5.maz")
     print(test_maze3)
     test_problem = SensorlessProblem(test_maze3)
-    print(test_problem)
+    test_problem.generate_start_state()
