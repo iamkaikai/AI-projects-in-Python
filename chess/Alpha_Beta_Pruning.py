f12da1c3d837ca0089f4898b381ddf6b56849e8d
Kyle Huang <iamkaikai@livemail.tw>
2023-10-04 00:28:43 -0400

    removing redundancy

--- chess/Alpha_Beta_Pruning.py
+++ chess/Alpha_Beta_Pruning.py
@@ -18,20 +18,43 @@ class A_B_Pruning():
         if board.can_claim_fifty_moves():
             return True
         
-        return depth == 0
+        if  board.is_repetition(3) or board.can_claim_threefold_repetition():
+            return True
+    
+        if depth == 0:
+            return True
     
     def board_hash(self, board):
             return hash(str(board))
         
-    def heuristic_sort(self, board, move):
+    def evaluate_move_score(self, board, move):
+        score = 0
+        if board.is_capture(move):
+            piece_values = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 1000}
+            
+            if board.piece_at(move.to_square):
+                captured_piece = board.piece_at(move.to_square).symbol().upper()
+                captured_value = piece_values.get(captured_piece)
+                capturing_piece = board.piece_at(move.from_square).symbol().upper()
+                capturing_value = piece_values.get(capturing_piece)    
+                if captured_piece and captured_value:
+                    score += captured_value*10
+                if captured_value and capturing_value:
+                    score += 10 * (captured_value - 0.5 * capturing_value)
+            
         board.push(move)
-        value = evalute_move(board)
+        score += 0.1 * board.legal_moves.count() ** 0.5
         board.pop()
-        return value
+        
+        board.push(move)
+        if board.is_checkmate():
+            score += 1000
+        elif board.is_check():
+            score += 500
+        board.pop()
 
-    def sort_move(self, board):
-        moves = list(board.legal_moves)
-        return sorted(moves, key=lambda move: -self.heuristic_sort(board, move))
+        return score
+
         
     def minimax(self, board, depth, alpha, beta, maxPlayer):
         self.visited_nodes +=1
@@ -39,18 +62,19 @@ class A_B_Pruning():
         
         # if seen the move before with same ev, skip search
         if board_key in self.transposition_table:
-            stored_depth, stored_value = self.transposition_table[board_key]
-            if stored_depth <= depth:
-                return stored_value
+            stored_value = self.transposition_table[board_key]
+            return stored_value
         
         # base case
         if self.cutoff_test(board, depth):
             return evalute_move(board)
         
+        legal_moves = list(board.legal_moves)
+        sorted_moves = sorted(legal_moves, key=lambda move: self.evaluate_move_score(board, move), reverse=True)
+            
         if maxPlayer:
             maxEV = -float('inf')
-            moves = self.sort_move(board)
-            for move in moves:
+            for move in sorted_moves:
                 board.push(move)
                 eval = self.minimax(board, depth-1, alpha, beta, False)
                 board.pop()
@@ -58,12 +82,11 @@ class A_B_Pruning():
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
-            self.transposition_table[board_key] = (depth, maxEV)
+            self.transposition_table[board_key] = maxEV
             return maxEV
         else:
             minEV = float('inf')
-            moves = self.sort_move(board)
-            for move in moves:
+            for move in sorted_moves:
                 board.push(move)
                 eval = self.minimax(board, depth-1, alpha, beta, True)
                 board.pop()
@@ -71,7 +94,7 @@ class A_B_Pruning():
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
-            self.transposition_table[board_key] = (depth, minEV)
+            self.transposition_table[board_key] = minEV
             return minEV
         
     def choose_move(self, board):
@@ -88,6 +111,7 @@ class A_B_Pruning():
                 best_move = move
                 best_ev = ev
             alpha = max(alpha, ev)
+
         print("Alpha-Beta Pruning AI recommending move " + str(best_move))        
         print(f"visited nodes = {self.visited_nodes}")
         print(f'best_ev = {best_ev}')
--- chess/evalute_move.py
+++ chess/evalute_move.py
@@ -1,44 +1,43 @@
 import chess
 
 def evalute_move(board):
-        if board.is_checkmate():
-            return float('inf') if board.turn else -float('inf')
-        
-       
-        values = {
-            'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 1000,
-            'p': -1, 'n': -3, 'b': -3, 'r': -5, 'q': -9, 'k': -1000
-        }
-        
-        piece_weights = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9}
-        
-        # sum up all the pieces for total ev
-        total_pieces_value = 0
-        defense_penalty = 0
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            piece_value = values.get(str(piece))
-            if piece and piece_value:
-                total_pieces_value += piece_value
+    total = 0
+    if board.is_checkmate():
+        return float('inf') if board.turn else -float('inf')
+    
+    if board.is_repetition(3) or board.can_claim_threefold_repetition() or board.can_claim_fifty_moves() or board.is_fifty_moves():
+        total -= 999
+    
+    values = {
+        'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 1000,
+        'p': -1, 'n': -3, 'b': -3, 'r': -5, 'q': -9, 'k': -1000
+    }
+    
+    piece_weights = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9}
+    
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        piece_value = values.get(str(piece))
+        if piece and piece_value:
+            total += piece_value
+            if board.is_attacked_by(not board.turn, square):
+                total -= abs(piece_value)
                 
-                # penalty for being attacked    
-                if board.is_attacked_by(not board.turn, square):
-                        defense_penalty -= abs(piece_value)
-                    
-        # Check if the piece can capture an enemy piece
-        capture_bonus = 0
-        mobility_bonus = 0
-        legal_moves = list(board.legal_moves)        
-        for move in legal_moves:
-            if board.is_capture(move):
-                capturing_piece = board.piece_at(move.from_square)
-                target_piece = board.piece_at(move.to_square)
-                if capturing_piece and target_piece:
-                    capture_bonus += abs(values[str(target_piece)]) - abs(values[str(capturing_piece)])
+    legal_moves = list(board.legal_moves)        
+    for move in legal_moves:
+        if board.is_capture(move):
+            capturing_piece = board.piece_at(move.from_square)
+            target_piece = board.piece_at(move.to_square)
+            if capturing_piece and target_piece:
+                total += abs(values[str(target_piece)]) - abs(values[str(capturing_piece)]) * 0.5
                 
-            moving_piece = board.piece_at(move.from_square)
-            moving_value = piece_weights.get(str(moving_piece))
-            if moving_piece and moving_value:
-                mobility_bonus += moving_value
-            
-        return total_pieces_value + capture_bonus + defense_penalty + mobility_bonus
+        moving_piece = board.piece_at(move.from_square)
+        moving_value = piece_weights.get(str(moving_piece))
+        if moving_piece and moving_value:
+            total += moving_value
+    
+    center_squares = [chess.D4, chess.D5, chess.E4, chess.E5]
+    if move.to_square in center_squares:
+        total += 0.5
+
+    return total
