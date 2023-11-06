603bac59f81420aec3a2327d5d2242b0f04817a3
iamkaikai <iamkaikai@livemail.tw>
2023-11-06 14:26:57 -0500

    adding HMM features

--- HHM/hmm.py
+++ HHM/hmm.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 class Maze:
     def __init__(self):
@@ -76,18 +77,75 @@ class Robot:
         for color in colors:
             self.sensor_model[color] = {c: 0.88 if c == color else 0.04 for c in colors}
         print(f'\nsensor_model{self.sensor_model}')
+
+    def print_best_guest(self, row, col):
+        new_maze = Maze()
+        new_maze.maze = copy.deepcopy(self.maze.maze)
+        new_maze.width = self.maze.width
+        new_maze.height = self.maze.height
+        new_maze.maze[row][col] = '●'
+        
+        # Split the string representations of the mazes into lists of lines
+        map_display = ''
+        for idx_row in range(self.maze.height):
+            original_line = ' '.join(self.maze.maze[idx_row])
+            new_line = ' '.join(new_maze.maze[idx_row])
+            combined_line = original_line + ' ' * 4 + new_line
+            map_display += combined_line + '\n'
+        print(f'\n{map_display}')
+
+        
+    def position_to_belief_state(self, transition_model):
+        return {(row, col): index for index, (row, col) in enumerate(transition_model)}
+    
+    def belief_state_to_position(self, index):
+        for idx, pos in enumerate(self.transition_model):
+            if index == idx:
+                return pos
         
     def predict(self):
         new_belief_state = [0 for _ in range(len(self.belief_state))]
-        row_col_to_belief_state = {(row, col): index for index, (row, col) in enumerate(self.transition_model)}
-        print(f'row_col_to_belief_state: {row_col_to_belief_state}')
+        row_col_to_belief_state = self.position_to_belief_state(self.transition_model)
+        
+        for (row, col), next_directions_prob in self.transition_model.items():  # (1, 1): {'N': {(1, 1): 0.25}, ...''E': {(1, 2): 0.25}}
+            cur_index = row_col_to_belief_state[(row, col)]                     # (1, 1) - > 0
+            for next_direction, transitions in next_directions_prob.items():    # 'N': {(1, 1): 0.25}, ...'E': {(1, 2): 0.25}}
+                for (next_row, next_col), next_prob in transitions.items():     # (1, 1): 0.25
+                    next_index = row_col_to_belief_state[(next_row, next_col)]  
+                    if next_index is not None:
+                        new_belief_state[next_index] += self.belief_state[cur_index] * next_prob
+                    
+        self.belief_state = new_belief_state    
+                
+    def update(self, reading_color):
+        total_prob = 0
+        for index, (row, col) in enumerate(self.transition_model):
+            color = self.maze.maze[row][col]
+            self.belief_state[index] *= self.sensor_model[reading_color][color]
+            total_prob += self.belief_state[index]
+        
+        if total_prob != 1:
+            self.belief_state = [value / total_prob for value in self.belief_state]
         
     
-    def update(self):
-        pass
-    
-    def filter(self, sensor):
-        for color in sensor:
+    def filter(self, reading_seq):
+        for color in reading_seq:
+            color = color.upper()
             self.predict()
             self.update(color)
-            
+        return self.belief_state
+    
+    def sensor_read(self, color_seq):
+        max_prob = float('-inf')
+        best_guest_row, best_guest_col = None, None
+        
+        belief_state = self.filter(color_seq)
+        for idx, value in enumerate(belief_state):
+            row, col = self.belief_state_to_position(idx)
+            if belief_state[idx] > max_prob:
+                max_prob = belief_state[idx]
+                best_guest_row, best_guest_col = row, col
+            print(f'({row}, {col}) = {format(belief_state[idx], ".16f")}')
+        
+        print(f'\n👉 most likely position: \n({best_guest_row}, {best_guest_col}) = {max_prob}  #(row, col)')
+        self.print_best_guest(best_guest_row, best_guest_col)
--- HHM/problem.py
+++ HHM/problem.py
@@ -3,16 +3,20 @@ from hmm import Maze, Robot
 
 maze = Maze()
 maze.generate_rand_maze(4, 4)
-print(maze)
-
 maze.insert_wall_in_maze(4,2)
 maze.insert_wall_in_maze(1,3)
 maze.insert_wall_in_maze(2,3)
 maze.insert_wall_in_maze(2,2)
-print(maze)
+
+print(f'maze: \n{maze}')
+color_seq = input('please enter your color sequence (e.g. R,G,R,R): ')
+color_seq = color_seq.split(",")
 
 robot = Robot(maze)
 robot.generate_states()
 robot.generate_transition_model()
 robot.generate_sensor_model()
-robot.predict()
+
+print(f'\ninput sequence = {color_seq}\n')
+robot.sensor_read(color_seq)
+
